98d83242d571c0af04aa2a7bdf7552a00c6af583
Artur Kosmovskij <kosmos146139@gmail.com>
2024-03-30 21:04:32 +0300

    132

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,36 @@
 package com.epam.mjc.io;
 
+import javax.imageio.IIOException;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file) throws IOException {
+        String[] strings = null;
+
+        try(FileInputStream inputStream = new FileInputStream(file)) {
+            String text = null;
+            int ch;
+            while ((ch = inputStream.read()) != -1){
+                text += String.valueOf((char)ch);
+            }
+            text = text.replace("\n"," ");
+            text = text.replace("\r","");
+            strings = text.split(" ");
+
+        } catch (IOException e)
+        {
+
+        }
+        String name = strings[1];
+        Integer age = Integer.valueOf(strings[3]);
+        String email = strings[5];
+        Long phone = Long.valueOf(strings[7]);
+        Profile pro = new Profile(name,age,email,phone);
+        return pro;
     }
 }
